8ba196d30e0f9f996760b9284c2930e1e0cf3005
marclura <29801332+marclura@users.noreply.github.com>
2026-06-26 09:24:41 +0200

    add bet

--- src/js/scenes/bet.js
+++ src/js/scenes/bet.js
@@ -1,4 +1,5 @@
 import EventBus from '../core/EventBus.js'
+import Store from '../core/Store.js'
 
 let p1Ready = false
 let p2Ready = false
@@ -10,25 +11,100 @@ const handlers = {  // list of event listeners
 
 export const el = document.querySelector('#scene-bet')
 
+const betP1 = document.querySelector('#bet-p1')
+const betP2 = document.querySelector('#bet-p2')
+
 export function onEnter() {
     p1Ready = false
     p2Ready = false
 
+    Store.players[0].bet = 2
+    Store.players[1].bet = 2
+
+    betP1.textContent = betP2.textContent = `${Store.players[0].bet} → ${Store.players[0].bet * 2}`
+    betP2.textContent = betP2.textContent = `${Store.players[1].bet} → ${Store.players[1].bet * 2}`
+
+    // bet up
+    handlers.betUpP1 = () => {
+        if(Store.players[0].bet < 12) {
+            Store.players[0].bet += 1
+        }
+        betP1.textContent = `${Store.players[0].bet} → ${Store.players[0].bet * 2}`
+    }
+
+    handlers.betUpP2 = () => {
+        if(Store.players[1].bet < 12) {
+            Store.players[1].bet += 1
+        }
+        betP2.textContent = `${Store.players[1].bet} → ${Store.players[1].bet * 2}`
+    }
+
+    // bet down
+    handlers.betDownP1 = () => {
+        if(Store.players[0].bet > 2) {
+            Store.players[0].bet -= 1
+        }
+        betP1.textContent = `${Store.players[0].bet} → ${Store.players[0].bet * 2}`
+    }
+
+    handlers.betDownP2 = () => {
+        if(Store.players[1].bet > 2) {
+            Store.players[1].bet -= 1
+        }
+        betP2.textContent = `${Store.players[1].bet} → ${Store.players[1].bet * 2}`
+    }
+
+    // confirm
     handlers.confirmP1 = () => {
         p1Ready = true
-        if (p1Ready && p2Ready) EventBus.emit('scene:gamePlay')
+        if (p1Ready && p2Ready) EventBus.emit('scene:gamePlay');
+
+        [...document.querySelector('#scene-bet .controller-p1').children].forEach(el => {
+            el.style.display = 'none'
+        })
     }
     handlers.confirmP2 = () => {
         p2Ready = true
-        if (p1Ready && p2Ready) EventBus.emit('scene:gamePlay')
+        if (p1Ready && p2Ready) EventBus.emit('scene:gamePlay');
+
+        [...document.querySelector('#scene-bet .controller-p2').children].forEach(el => {
+            el.style.display = 'none'
+        })
     }
 
-    document.querySelector('#btn-play-p1').addEventListener('click', handlers.confirmP1)
-    document.querySelector('#btn-play-p2').addEventListener('click', handlers.confirmP2)
+    // bet up
+    document.querySelector('#scene-bet .btn-B-p1').addEventListener('click', handlers.betUpP1)
+    document.querySelector('#scene-bet .btn-B-p2').addEventListener('click', handlers.betUpP2)
+
+    // bet down
+    document.querySelector('#scene-bet .btn-A-p1').addEventListener('click', handlers.betDownP1)
+    document.querySelector('#scene-bet .btn-A-p2').addEventListener('click', handlers.betDownP2)
+
+    // confirm
+    document.querySelector('#scene-bet .btn-select-p1').addEventListener('click', handlers.confirmP1)
+    document.querySelector('#scene-bet .btn-select-p2').addEventListener('click', handlers.confirmP2)
 }
 
 
 export function onExit() {
-    document.querySelector('#btn-play-p1').removeEventListener('click', handlers.confirmP1)
-    document.querySelector('#btn-play-p2').removeEventListener('click', handlers.confirmP2)
+
+    // bet up
+    document.querySelector('#scene-bet .btn-B-p1').removeEventListener('click', handlers.betUpP1)
+    document.querySelector('#scene-bet .btn-B-p2').removeEventListener('click', handlers.betUpP2)
+
+    // bet down
+    document.querySelector('#scene-bet .btn-A-p1').removeEventListener('click', handlers.betDownP1)
+    document.querySelector('#scene-bet .btn-A-p2').removeEventListener('click', handlers.betDownP2)
+
+    // confirm
+    document.querySelector('#scene-bet .btn-select-p1').removeEventListener('click', handlers.confirmP1)
+    document.querySelector('#scene-bet .btn-select-p2').removeEventListener('click', handlers.confirmP2);
+
+    [...document.querySelector('#scene-bet .controller-p1').children].forEach(el => {
+        el.style.display = 'block'
+    });
+
+    [...document.querySelector('#scene-bet .controller-p2').children].forEach(el => {
+        el.style.display = 'block'
+    })
 }
